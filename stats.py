3596dcd82104ad450a8b88bcb84febce140c2186
nhanthanbich <152984069+nhanthanbich@users.noreply.github.com>
2025-07-05 16:13:52 +0700

    Create stats.py

---
+++ stats.py
@@ -0,0 +1,25 @@
+import random as rd
+
+species_base_stats = {
+    "Witch": {
+        "role": "Pháp sư, Cấu rỉa", "strength": 41, "stamina": 177,
+        "vitality": 711, "dexterity": 44, "agility": 15
+    },
+    "Vampire": {
+        "role": "Pháp sư", "strength": 67, "stamina": 54,
+        "vitality": 1455, "dexterity": 10, "agility": 20
+    },
+    "Werewolf": {
+        "role": "Sát thủ", "strength": 98, "stamina": 34,
+        "vitality": 819, "dexterity": 45, "agility": 29
+    },
+}
+
+def rand_stat(attr, base):
+    if attr == "vitality":
+        delta = 115
+    elif attr in ["dexterity", "agility"]:
+        delta = 5
+    else:
+        delta = 15
+    return max(0, base + rd.randint(-delta, delta))
